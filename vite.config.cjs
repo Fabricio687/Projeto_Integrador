7829e4b64fd70f385a86051fd41f4ac41a862059
Fabricio687 <santosdeandradefabricio18@gmail.com>
2025-11-10 16:39:37 -0300

    Add files via upload

--- vite.config.cjs
+++ vite.config.cjs
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg'], // ← ADICIONAR
+  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg'],
   server: {
     port: 5173,
     open: true,
@@ -14,5 +14,8 @@ export default defineConfig({
         secure: false,
       },
     },
+  },
+  build: {
+    outDir: 'dist',
   },
 });
